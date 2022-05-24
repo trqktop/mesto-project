28e14a76fda69ea181152ac801b1944dc8cfafe2
trqktop <trqktop@yandex.ru>
2022-05-24 15:28:25 +0300

    add popup openClose

--- pages/script.js
+++ pages/script.js
@@ -1,5 +1,11 @@
-let tes = 1+3;
-let al = tes + 222;
-console.log(tes);
-console.log(al);
-console.log(al+tes+"semp");
+let popup = document.querySelector('.popup');//попак редактировать профиль
+let closeEditProfile = document.querySelector('.popup__close-button');//кнопка -закрыть попап редактирования профиля
+let openEditProfile = document.querySelector('.profile__edit-button');//кнопка -открыть попап редактирования профиля
+
+
+openEditProfile.addEventListener('click', openClose);//отслеживаем клик и даем ссылку на обработчик
+closeEditProfile.addEventListener('click', openClose);//отслеживаем клик и даем ссылку на обработчик
+
+function openClose() {
+    popup.classList.toggle('popup_opened');
+} //обработчик открытия и закрытия попап
