a6da9555c5f3f17d5f60a69121df5c1ae19bfbc9
trqktop <trqktop@yandex.ru>
2022-05-29 12:13:58 +0300

    added feature add new photos on page

--- pages/script.js
+++ pages/script.js
@@ -1,6 +1,6 @@
-let popupEditProfile = document.querySelector('.popup');//попак редактировать профиль
-let closeEditProfile = popupEditProfile.querySelector('.popup__close-button');//кнопка -закрыть попап редактирования профиля
-let openEditProfile = document.querySelector('.profile__edit-button');//кнопка -открыть попап редактирования профиля
+const popupEditProfile = document.querySelector('.popup');//попак редактировать профиль
+const closeEditProfile = popupEditProfile.querySelector('.popup__close-button');//кнопка -закрыть попап редактирования профиля
+const openEditProfile = document.querySelector('.profile__edit-button');//кнопка -открыть попап редактирования профиля
 
 
 
@@ -16,24 +16,24 @@ function openClose() {
 
 
 
-let porfileName = document.querySelector('.profile__user-name');//имя пользователя на главной
+const porfileName = document.querySelector('.profile__user-name');//имя пользователя на главной
 
-let porfileAbout = document.querySelector('.profile__user-about');//профессия пользователя на главной
-
-
-let popupInputName = document.querySelector('#popupNameInput ');//ввод имя пользователя 
-let popupInputAbout = document.querySelector('#popupAboutInput');//ввод профессии пользователя
-
-let popupSubmit = document.querySelector('.popup__submit-button')//кнопка сохранить изменения в попапе редактирования профиля
+const porfileAbout = document.querySelector('.profile__user-about');//профессия пользователя на главной
 
 
+const popupInputName = document.querySelector('#popupNameInput ');//ввод имя пользователя 
+const popupInputAbout = document.querySelector('#popupAboutInput');//ввод профессии пользователя
+
+const popupSubmit = document.querySelector('.popup__submit-button')//кнопка сохранить изменения в попапе редактирования профиля
+
+
+popupInputName.value = porfileName.textContent;
+popupInputAbout.value = porfileAbout.textContent;
 
 
 popupSubmit.addEventListener('click', saveClose);//отслеживаем клик и даем ссылку на обработчик для сохранения инфы в попапе
 
 
-popupInputName.value = porfileName.textContent;
-popupInputAbout.value = porfileAbout.textContent;
 
 function saveClose(evt) {
     evt.preventDefault();
@@ -45,20 +45,85 @@ function saveClose(evt) {
 
 //нИЖЕ ПОПАП ДОБАВЛЕНИЯ НОВОГО МЕСТА НА СТРАНИЦЕ ПО АЙДИШНИКАМ
 
-let popupMesto = document.querySelector('#popupMesto')//попап редактирования места
+const popupMesto = document.querySelector('#popupMesto')//попап редактирования места
 
-let profileAddButton = document.querySelector('.profile__add-button')//кнопка открытия попапа редактирования места 
+const profileAddButton = document.querySelector('.profile__add-button')//кнопка открытия попапа редактирования места 
 
-let createMesto = document.querySelector('#createMesto')//кнопка сохранения места 
+const createMesto = document.querySelector('#createMesto')//кнопка сохранения места 
 
-let mestoCloseButton = document.querySelector('#mestoCloseButton');
+const mestoCloseButton = document.querySelector('#mestoCloseButton');
 
-mestoCloseButton.addEventListener('click',addMestoOpen)//СОБЫТИЕ ЗАКРЫТИЯ ПОПАПА МЕСТО
-createMesto.addEventListener('click',addMestoOpen)//событие кнопки сохранить
+mestoCloseButton.addEventListener('click', addMestoOpen)//СОБЫТИЕ ЗАКРЫТИЯ ПОПАПА МЕСТО
+createMesto.addEventListener('click', addMestoOpen)//событие кнопки сохранить
 
-profileAddButton.addEventListener('click',addMestoOpen)//событие кнопки открыть попап редактирования места
+profileAddButton.addEventListener('click', addMestoOpen)//событие кнопки открыть попап редактирования места
 
 
-function addMestoOpen(){
+function addMestoOpen() {
     popupMesto.classList.toggle('popup_opened');
-}//ОБРАБОТЧИК ЗАКРЫТТИЯ ПОПАПА
+}//Открытие и закрытие попапа
+
+
+//Шесть карточек «из коробки»
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
+
+//добавляем новое место по кнопке добавить
+
+
+
+
+createMesto.addEventListener('click', addNewMesto)
+
+function addNewMesto(evt) {
+    evt.preventDefault();
+
+    let elementGridContainer = document.querySelector('.elements__grid-container')//нашел контейнер
+    let elementCard = document.querySelector('.element')//нашел елемент
+    let elementNewCardContainer = document.createElement('li')//добавляю елемент в контейнер
+    let elementNewCard = document.createElement('figure')//создал карточку для заливки в нее стилей
+    elementNewCard.innerHTML = elementCard.innerHTML //скопировал каркас хтмл с готовой карточки
+    elementNewCard.classList.add('element') //добавил класс для подтягивания стилей
+    elementGridContainer.prepend(elementNewCardContainer) //добавляю на страницу елемент при клике 
+    elementNewCardContainer.append(elementNewCard) //добавляю на страницу елемент при клике 
+
+
+    let urlImageInput = document.querySelector('#urlImageInput')//ищем строку ввода ссылки на картинку
+    let elementCardImage = elementNewCardContainer.querySelector('.element__image')//ищем в созданой выше карточке класс картинки
+    elementCardImage.setAttribute('src', urlImageInput.value) //передаем с инпута в картинку ссылку
+
+    let elementCaptionAbout = elementNewCardContainer.querySelector('.element__caption-about') //ищем в созданой карточке загловок
+    let nameImageInput = document.querySelector('#nameImageInput') //ищем строку ввода для заголовка
+    elementCaptionAbout.textContent = nameImageInput.value //передаем значение строки ввода в заголовок
+
+
+}
+
+
+
